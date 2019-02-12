5e75f61e37501661b111f6f2ab15618dfed3ea6a
Miguel Miranda <mikemp96@gmail.com>
2019-02-11 18:40:22 -0600

    Create models

---
+++ internal/api/models/geodata.go
@@ -0,0 +1,17 @@
+package models
+
+// Hotspot is a hotspot object
+type Hotspot struct {
+	ID     int        `json:"id"`
+	Name   string     `json:"name"`
+	Type   string     `json:"type"`
+	Coords [2]float64 `json:"coords"`
+}
+
+// Category is a category object
+type Category struct {
+	ID       int        `json:"id"`
+	Name     string     `json:"name"`
+	Active   bool       `json:"active"`
+	Hotspots []*Hotspot `json:"features"`
+}
