a59e6976aa7b49ae999377b9d4d3911fb92e9a37
Chuy Alvarez <jesusaagomez@hotmail.com>
2019-03-25 16:54:08 -0600

    added preflight handling and credential use (#11)

--- internal/api/middleware/cors.go
+++ internal/api/middleware/cors.go
@@ -12,8 +12,14 @@ func CORS(origin string) alice.Constructor {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 			h.ServeHTTP(w, r)
 		})
 	}
